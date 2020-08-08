9c6f4614a656794c61b2d9d09e70410f65afa652
Priyavardhan <priyavardhan@pop-os.localdomain>
2020-08-08 12:56:08 +0530

    js

---
+++ js/register.js
@@ -0,0 +1,41 @@
+function register(){
+    var name = document.getElementById("name").value;
+    var username = document.getElementById("username").value;
+    var pass = document.getElementById("pass").value;
+    var con_pass = document.getElementById("con_pass").value;
+    var email = document.getElementById("email").value;
+    var contact = document.getElementById("contact").value;
+
+
+
+    if ( name=="" )
+        alert("Enter Name");
+
+    else if ( username=="" )
+        alert("Enter Username");
+
+    else if ( pass=="" )
+        alert("Enter Password");
+
+    else if ( con_pass=="" )
+        alert("Confirm Password");
+
+    else if ( con_pass != pass )
+        alert("Password donot match");
+    
+    else if ( email=="" )
+        alert("Enter E-mail");
+
+    else if ( contact=="" )
+        alert("Enter Contact no");
+    
+    
+    else if (contact.length!=10 )
+        alert("Enter valid Contact no."); 
+
+    else 
+    {
+        alert("Registration successful.\nName : " + name + "\nUsername : " + username + "\nE-mail : " + email + "\nContact No. : " + contact );
+        window.location = "dashboard.html";
+    }
+}
